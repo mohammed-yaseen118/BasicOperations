b629d759f51448c7b16dd2fd2422d1a8e825d4eb
mohammed-yaseen118 <yaseenhulgur@gmail.com>
2025-05-06 15:51:53 +0530

    file saved

---
+++ BasicOperations.java
@@ -0,0 +1,14 @@
+// Removed package declaration to match the default package
+
+public class BasicOperations {
+    public static void main(String [] args){
+        int a = 15;
+        int b = 5;
+        int sum = a+b;
+        int diff = a-b;
+        int prod = a*b;
+        System.out.println("Addition:"+sum);
+        System.out.println("Difference:"+diff);
+        System.out.println("Product:"+prod);
+    }
+}
